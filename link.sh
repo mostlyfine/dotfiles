17240279dfc45d436c59173f78cfdc957c88af9c
Seiji Sawayanagi <mostlyfine@gmail.com>
2014-11-05 18:09:49 +0900

    Add initial setup file.

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshenv ~/.zshenv
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.pryrc ~/.pryrc
