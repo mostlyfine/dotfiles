d9d34a8fdc98ac6c47506e710e4af0d310bb5f6a
Seiji Sawayanagi <mostlyfine@gmail.com>
2017-05-11 17:43:07 +0900

    Clone after directory creation.

--- link.sh
+++ link.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshenv ~/.zshenv
@@ -8,3 +7,5 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.pryrc ~/.pryrc
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
