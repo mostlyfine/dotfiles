03eaa8b46df0ad769dd937434feff95892233455
Seiji Sawayanagi <seiji.sawayanagi@dena.com>
2022-10-24 17:58:25 +0900

    refactoring

--- .vim/template/template.py
+++ .vim/template/template.py
@@ -3,19 +3,34 @@
 Module Docstring
 """
 
+import sys
 from logging import getLogger, DEBUG, basicConfig
 
 basicConfig(level=DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 logger = getLogger(__name__)
 
 class TestClass:
-    def __init__(self, msg='hello world'):
-        self.message = msg
+    __message: str
 
-    def main(self):
-        logger.info(self.message)
+    def __init__(self, name: str = 'world') -> None:
+        self.__message = f'hello {name}'
+
+    def main(self) -> None:
+        logger.info(self.get_message())
+
+    def get_message(self) -> str:
+        return self.__message
 
 
 if __name__ == "__main__":
-    test = TestClass()
-    test.main()
+    if len(sys.argv) > 1:
+        test = TestClass(sys.argv[1])
+    else:
+        test = TestClass()
+
+    try:
+        print(test.__message)
+    except Exception as e:
+        #logger.error(e)
+        test.main()
+
